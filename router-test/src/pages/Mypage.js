342b22e07d71575933418d075c2b0b218f56c1f6
zu0p <a33a66a99@naver.com>
2019-05-15 00:23:49 +0900

    mypage

--- router-test/src/pages/Mypage.js
+++ router-test/src/pages/Mypage.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import './Mypage.css';
 
 const Mypage = () => {
-    return(
+    return (
         <div>
-            <div className="">
+            <div className="husks">
+                <div className="mypage_container">
+                    <div className="mypage_banner">
+                        <p>Edit your profile!</p>
+                        <hr />
+                    </div>
+
+                    <div className="user_info">
+                        <div className="user_info_text">
+                            <div className="user_info_text_item">
+                                <div><label>Full name</label></div>
+                                <input type="text" name="input_name"></input>
+                            </div>
+
+                            <div className="user_info_text_item">
+                                <div><label>Nickname</label></div>
+                                <input type="text" name="input_nickname"></input>
+                                <button className="check_overlap_btn">중복확인</button>
+                            </div>
+
+                            <div className="user_info_text_item">
+                                <div><label>Password</label></div>
+                                <input type="password" name="input_pw"></input>
+                                <input type="password" name="check_input_pw"></input>
+                                <button className="check_pw_btn">비밀번호 확인</button>
+                            </div>
+                        </div>
+
+                        <div className="user_info_picture">
+                            <h3>사진</h3>
+                        </div>
+                    </div>
+
+                    <div className="change_bt_body">
+                        <button className="user_info_change_btn">Save changes!</button>
+                    </div>
+                </div>
             </div>
         </div>
     );
--- router-test/src/shared/App.js
+++ router-test/src/shared/App.js
@@ -9,40 +9,42 @@ import DirectMessage_Nav from 'components/DirectMessage_Nav';
 import './App.css';
 
 class App extends Component {
-    render() {
-        return(
-          <div>
-            <Header />            
-            <div className="left_container">
-              <div className="client_channels_list_container">
-                {/* <div className="groups_nav_container">
+  render() {
+    return (
+      <div className="zu0p">
+        <Header />
+        <div className="my_content">
+          <div className="left">
+            <div className="client_channels_list_container">
+              {/* <div className="groups_nav_container">
                   <Groups_Nav />
                 </div> */}
-                <div className="menu_nav_container">            
-                  <Menu />
-                  <hr width="140px" />
-                  <Channels_Nav />
-                  <hr width="140px" />
-                  <DirectMessage_Nav />
-                </div>
-              </div>
-
-              <div className="client_main_container">              
-                <Route exact path="/" component={Home} />
-                <Switch>
-                  <Route path="/about/:name" component={About} />
-                  <Route path="/about" component={About} />
-                </Switch>
-                <Route path="/posts" component={Posts} />
-                <Route path="/myPage" component={Mypage} />
-                <Route path="/channels" component={Channels} />
-                <Route path="/conference" component={Conference} />
-                <Route path="/direct" component={Direct} />
+              <div className="menu_nav_container">
+                <Menu />
+                <hr width="140px" />
+                <Channels_Nav />
+                <hr width="140px" />
+                <DirectMessage_Nav />
               </div>
             </div>
+
+            <div className="client_main_container">
+              <Route exact path="/" component={Home} />
+              <Switch>
+                <Route path="/about/:name" component={About} />
+                <Route path="/about" component={About} />
+              </Switch>
+              <Route path="/posts" component={Posts} />
+              <Route path="/myPage" component={Mypage} />
+              <Route path="/channels" component={Channels} />
+              <Route path="/conference" component={Conference} />
+              <Route path="/direct" component={Direct} />
+            </div>
           </div>
-        );
-    }
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
