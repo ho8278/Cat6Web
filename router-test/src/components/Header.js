c384512ee1bbb54853f57b85a7f6fd0a84e55083
zu0p <a33a66a99@naver.com>
2019-05-16 21:58:52 +0900

    mypage_서버호출1

--- router-test/src/components/Header.js
+++ router-test/src/components/Header.js
@@ -1,39 +1,80 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { NavLink } from 'react-router-dom';
+import { Popup } from 'react-popup';
 import './Header.css';
 import CatSixLogo from 'img/CatSix.jpg';
 import MyPageLogo from 'img/MyPageLogo.jpg';
 
-const Header = () => {
+// class PopupZ extends Component {
+//     render() {
+//         return (
+//             <div className='popup'>
+//                 <div className='popup_inner'>
+//                     <h3>{this.props.text}</h3>
+//                     <input type="text" width="100px"></input>
+//                     <div>
+//                         <button onClick={this.props.closePopup}>Create</button>
+//                     </div>
+//                 </div>
+//             </div>
+//         );
+//     }
+// }
 
-    return(
-        <div className="head">
-            <NavLink to="/" > 
-            <img src={CatSixLogo} className="CatSixLogoStyle"/>
-            </NavLink>
-            <div className="dropdown" >
-            <img src={MyPageLogo} className="MyPageLogoStyle"/>
-                <div class="dropdown-content">
-                    <NavLink to="/mypage">회원정보 수정</NavLink>
-                    <button className="a">로그아웃</button>
-                    <hr />
+class Header extends Component {
+    // constructor(){
+    //     super();
+    //     this.state={showPopup:false};
+    // }
 
-                    <div className="group_invite">
-                      <p className="groups_bar">Groups</p>
-                      <button className="add_btn" title="그룹 생성">+</button>
+    // togglePopup() {
+    //     this.setState({
+    //       showPopup: !this.state.showPopup
+    //     });
+    // }
+
+    LogoutPopup(){
+        alert('로그아웃 되었습니다!');
+        window.location='/';
+    }
+
+    render(){
+        return(
+            <div className="head">
+                <NavLink to="/" > 
+                <img src={CatSixLogo} className="CatSixLogoStyle"/>
+                </NavLink>
+                <div className="dropdown" >
+                <img src={MyPageLogo} className="MyPageLogoStyle"/>
+                    <div className="dropdown-content">
+                        <NavLink to="/mypage">회원정보 수정</NavLink>
+                        <button className="a" onClick={this.LogoutPopup}>로그아웃</button>
+                        <hr />
+    
+                        <div className="group_invite">
+                          <p className="groups_bar">Groups</p>
+                          <button className="add_btn" title="그룹 생성">+</button>
+                          {/* <button className="add_btn" title="그룹 생성" onClick={this.togglePopup.bind(this)}>+</button>                          */}
+                        </div>
+                        {/* {this.state.showPopup ?
+                            <PopupZ 
+                                text="New group name:"
+                            />
+                            :null
+                          } */}
+                        <NavLink to="/">group1</NavLink>
+                        <NavLink to="/">group2</NavLink>
+                        <NavLink to="/">group3</NavLink>    
+                        <hr />
+                        
+                        <button className="a">회원탈퇴</button>
                     </div>
-                    <NavLink to="/">group1</NavLink>
-                    <NavLink to="/">group2</NavLink>
-                    <NavLink to="/">group3</NavLink>    
-                    <hr />
-                    
-                    <button className="a">회원탈퇴</button>
                 </div>
+                <hr />
             </div>
-            <hr />
-        </div>
-       
-    );
-};
+           
+        );
+    }
+}
 
 export default Header
--- router-test/src/pages/Mypage.js
+++ router-test/src/pages/Mypage.js
@@ -1,51 +1,101 @@
 import React from 'react';
+import { Component } from 'react';
+import axios from 'axios';
 import './Mypage.css';
 
-const Mypage = () => {
-    return (
-        <div>
-            <div className="husks">
-                <div className="mypage_container">
-                    <div className="mypage_banner">
-                        <p>Edit your profile!</p>
-                        <hr />
-                    </div>
+class Mypage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            data: [],
+            result: null
+        }
+    }
+    
+    componentDidMount() {
+        fetch("http://180.71.228.163:8080/showClientInfo?client_ID="+"a33a66a99")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        data: result.data,
+                        result: result.result
+                    });
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+            .then(function (response){
+                console.log(response);
+            })
+    }
 
-                    <div className="user_info">
-                        <div className="user_info_text">
-                            <div className="user_info_text_item">
-                                <div><label>Full name</label></div>
-                                <input id="items" type="text" name="input_name"></input>
+    render() {
+        const { error, isLoaded, data } = this.state;
+        if (error) {
+            return <div><h1>Error:</h1>{error.message}</div>;
+        }
+        else if (!isLoaded) {
+            return (
+                <div>
+                    <div><h1>Loading...</h1></div>
+                </div>
+            );
+        }
+        else {
+            return (
+                <div>
+                    <div className="husks">
+                        <div className="mypage_container">
+                            <div className="mypage_banner">
+                                <p>Edit your profile!</p>
+                                <hr />
                             </div>
 
-                            <div className="user_info_text_item">
-                                <div><label>Nickname</label></div>
-                                <input id="items" type="text" name="input_nickname"></input>
-                                <button className="check_overlap_btn">중복확인</button>
-                            </div>
+                            <div className="user_info">
+                                <div className="user_info_text">
+                                    <div className="user_info_text_item">
+                                        <div><label>Full name</label></div>
+                                        <input id="items" type="text" name="input_name" placeholder={data.client_name}></input>
+                                    </div>
 
-                            <div className="user_info_text_item">
-                                <div><label>Password</label></div>
-                                <input id="items" type="password" name="input_pw"></input>
-                                <div>
-                                    <input id="items" type="password" name="check_input_pw"></input>
-                                    <button className="check_pw_btn">비밀번호 확인</button>
+                                    <div className="user_info_text_item">
+                                        <div><label>Nickname</label></div>
+                                        <input id="items" type="text" name="input_nickname" placeholder={data.client_nickname}></input>
+                                        <button className="check_overlap_btn">중복확인</button>
+                                    </div>
+
+                                    <div className="user_info_text_item">
+                                        <div><label>Password</label></div>
+                                        <input id="items" type="password" name="input_pw"></input>
+                                        <div>
+                                            <input id="items" type="password" name="check_input_pw"></input>
+                                            <button className="check_pw_btn">비밀번호 확인</button>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="user_info_picture">
+                                    <h3>사진</h3>
                                 </div>
                             </div>
-                        </div>
 
-                        <div className="user_info_picture">
-                            <h3>사진</h3>
+                            <div className="change_bt_body">
+                                <button className="user_info_change_btn">Save changes!</button>
+                            </div>
                         </div>
-                    </div>
-
-                    <div className="change_bt_body">
-                        <button className="user_info_change_btn">Save changes!</button>
                     </div>
                 </div>
-            </div>
-        </div>
-    );
+            );
+        }
+    }
 };
 
 export default Mypage;
