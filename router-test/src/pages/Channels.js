b61c25b981a80298de4c99467660cc71a45613e4
zu0p <a33a66a99@naver.com>
2019-05-27 03:38:14 +0900

    channel수정중

--- router-test/src/pages/Channels.js
+++ router-test/src/pages/Channels.js
@@ -12,8 +12,21 @@ class Channels extends Component {
       this.state = {
         logs: [],
         name:'',
-        roomId:'main'
+        roomId:'main',
+
+        //소속 그룹
+        teams: [],
+        s_team_id: '',
+        isClicked_t: false,
+
+        //소속 채팅방
+        chat_rooms: [],
+        s_chat_id: '',
+        s_chat_name: '',
+        isClicked_c: false
       }
+      this.input_chat_name = React.createRef()
+      this.add_chat = this.add_chat.bind(this)
     }
     
     // 컴포넌트가 마운트됐을 때 --- (※5)
@@ -27,6 +40,21 @@ class Channels extends Component {
         this.setState({logs: logs2}) 
         
       })
+
+      const id = window.sessionStorage.getItem('id');
+
+      fetch("http://180.71.228.163:8080/viewTeams/n?client_ID="+id)
+          .then(res => res.json())
+          .then(
+              (res) => {           
+                  this.setState({
+                      teams: res.data
+                  })
+              },
+              (error) => {                    
+                  alert(error);
+              }
+          )
     }
 
    componentDidUpdate()
@@ -39,6 +67,62 @@ class Channels extends Component {
       this.setState({name: e.target.value})
     }
   
+    setSelectedID(id){
+      this.setState({
+        s_team_id:id
+      })
+      const c_id = window.sessionStorage.getItem('id')
+
+      fetch("http://180.71.228.163:8080/viewChatRooms/n?client_ID="+c_id+"&team_ID=" + id)
+            .then(res => res.json())
+            .then(
+                (res) => {           
+                    this.setState({
+                        chat_rooms: res.data,
+                        isClicked_t: true
+                    })
+                },
+                (error) => {                    
+                    alert(error);
+                }
+            )
+    }
+
+    setChatID(chat_id, chat_name){
+      this.setState({
+        s_chat_id: chat_id,
+        s_chat_name: chat_name,
+        isClicked_c: true
+      })
+
+      //해당 채팅방 로그 띄우기
+    }
+
+    add_chat(e){
+      const c_id = window.sessionStorage.getItem('id')
+
+      if(this.input_chat_name.current.value ==''){
+        alert('생성할 채팅방 이름을 기입하세요')
+        e.preventDefault()
+        return;
+      }
+
+      fetch("http://180.71.228.163:8080/createChatRoom/n?client_ID="+c_id
+      +"&chat_room_name="+this.input_chat_name.current.value
+      +"&team_ID="+this.state.s_team_id,
+      {
+        method: "POST"
+      })
+            .then(
+                (res) => {    
+                  alert('채팅방 생성')
+                },
+                (error) => {                    
+                    alert(error);
+                }
+            )
+    }
+
     render () {
       // 로그를 사용해 HTML 요소 생성 --- (※6)
       const messages = this.state.logs.map(e => (
@@ -49,15 +133,47 @@ class Channels extends Component {
           <p style={{clear: 'both'}} />
         </div>   
       ))
+
+      let t_list = this.state.teams
+      let c_list = this.state.chat_rooms
+
+      // let check_1 = this.state.isClicked_t
+      // let check_2 = this.state.is
       
       return (
         <div>
-          <div className='chat-head'>
-          <p className='text'>메인 채팅방</p>
-          <p className="text"> 이름: </p><input value={this.state.name} className="NameBoxIn" onChange={e => this.nameChanged(e)}/>       
-          </div>         
-          <div id='ChatBox'>{messages}</div>
-          <ChatForm name={this.state.name} roomId={this.state.roomId} socket={socket}  />
+          <div className="zu0p">
+            <div className="select_group">
+              {t_list.map((name) => {
+                return <button onClick={(e) => this.setSelectedID(name.team_ID, e)}>
+                  {name.team_name}
+                </button>
+              })}
+              <hr />
+            </div>
+
+            <div className="select_chat">
+              {c_list.map((name) => {
+                return <button onClick={(e) => this.setChatID(name.chat_room_ID, name.chat_room_name, e)}>
+                  {name.chat_room_name}
+                </button>
+              })}
+              <input type="text" ref={this.input_chat_name} placeholder="Create new chatting room!"/>
+              <button id="add_chatroom" onClick={this.add_chat}>add</button>
+              <hr />
+            </div>
+          </div>
+
+
+          {/* chat-container */}
+          <div className="ki">
+            <div className='chat-head'>
+              <p className='text'>메인 채팅방</p>
+              <p className="text"> 이름: </p><input value={this.state.name} className="NameBoxIn" onChange={e => this.nameChanged(e)} />
+            </div>
+            <div id='ChatBox'>{messages}</div>
+            <ChatForm name={this.state.name} roomId={this.state.roomId} socket={socket} />
+          </div>
         </div>
       )
     }
